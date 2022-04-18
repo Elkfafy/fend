8f68766a4fc1c28ac57c7048d693345a8ea111b3
Ahmed Elkfafy <73217710+darkerfreelancer@users.noreply.github.com>
2022-04-18 18:18:12 +0200

    can i commit?

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,78 +1,71 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+/* getting some global elements */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+const ul = document.querySelector('#navbar__list');         // The main container of nav links
+const sections = document.querySelectorAll('section');      // All sections in the file.
 
-/**
- * Define Global Variables
- * 
-*/
-const ul = document.querySelector('#navbar_list');
-const fragment = document.createDocumentFragment();
-const sections = document.querySelectorAll('section');
+/***********************************/
+/* building some helper functions */
+/*********************************/
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+// building the nav anchors click handler
+const clickHandler = evnt => {
+    evnt.preventDefault();                                                              // prevent the links from their functionality
+    const section = document.querySelector('#' + evnt.currentTarget.classList[1]);      // select the section of the link
+    section.scrollIntoView({                                                            // scroll the view into that section
+        behavior: 'smooth'
+    });
+};
 
 // building the nav bar function
-const buildNav = (outer, data) => {
-    const fragment = document.createDocumentFragment();
-    data.forEach((info) => {
-        const li = document.createElement('li');
-        const a = document.createElement('a');
-        a.href = '#' + info.getAttribute('id');
-        a.textContent = info.getAttribute('data-nav');
-        li.appendChild(a);
-        fragment.appendChild(li);
-    });
-    outer.appendChild(fragment);
-}
+const buildNav = (container, sections) => {
+    const fragment = document.createDocumentFragment();     // Creating Fragment
+    sections.forEach((section) => {                         // iterating on every section in sections
+        const li = document.createElement('li');            // creating 'li' element
+        const a = document.createElement('a');              // creating 'a' element
+        const ID = section.getAttribute('id');              // select the 'id' attribute of the section
+        const NAME = section.getAttribute('data-nav');      // select the 'data-nav' attribute of the section
+        a.href = '#' + ID;                                  // give the 'a' element href attribute the link to the section
+        a.classList.add("menu__link");                      // give the 'a' element the style
+        a.classList.add(ID);                                // give the 'a' element a class that is specefic to the section that it points to.
+        a.textContent = NAME;                               // setting the 'a' element its text content.
+        a.addEventListener('click', clickHandler);          // handling click on the 'a' element
+        li.appendChild(a);                                  // append the 'a' element to the 'li' element.
+        fragment.appendChild(li);                           // append the 'li' element to the FRAGMENT.
+    }); 
+    
+    container.appendChild(fragment);                        // rebuild the website with the new content
+};
 
-// Adding class active to section when near top function
-const checkActive()
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+// getting a section from iteration for all sections and add 'active' class to link and 'animate-active' class to the section if the section in the view.
+const activeHandler = (section) => {
+    const TOP = section.getBoundingClientRect().top;    // select the top of the section.
+    
+    const ID = section.getAttribute('id');              // read the id value of the section.
+    const anchor = document.querySelector('.' + ID);    // select the anchor with the class value of the id of the section.
+    
+    // check if the TOP value in specific range : if True : the section in the view : if False : the section isn't in the view.
+    if (TOP > -10 && TOP <= 400) {
+        // if True : add class 'animate-active'(class that highlights and makes animation to the section) to the section and add class 'active'(class that highlights the anchor) to the anchor that points to the section.
+        section.classList.add('animate-active');
+        anchor.classList.add('active');
+    } else {
+        // if False : remove class 'animate-active'(class that highlights and makes animation to the section) to the section and remove class 'active'(class that highlights the anchor) to the anchor that points to the section.
+        section.classList.remove('animate-active');
+        anchor.classList.remove('active');
+    }
+};
 
-// build the nav
+// adding 'active' class to the section when it beacome near of top of viewport.
+const makeActive = evnt => {
+    const sectionLinks = document.querySelectorAll('#navbar__list li a');   // select all links of the nav bar
+    sections.forEach(activeHandler);                                        // iterating for each link and handle each iterate with 'activeHandler'.
+};
+
+/* Main Functions */
+
+// build the nav bar
 buildNav(ul, sections);
 
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
+// Add event listner to scrolling of the page making if section in a view make it having animation and its button to highlight.
+window.addEventListener('scroll', makeActive);
 
